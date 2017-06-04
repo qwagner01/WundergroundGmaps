9342487f09a455b70350457424d9cc2f14b7dae4
Quinn Wagner <Quinn@Kerrys-MBP-3.home>
2017-06-03 20:14:44 -0400

    My first commit

--- public/myScript.js
+++ public/myScript.js
@@ -1,70 +1,86 @@
 var button = document.getElementById("button");
 var display = document.getElementById("info");
-button.addEventListener('click', goGetTheInfo);
+var button2 = document.getElementById('button2');
 var coord;
+// var content;
+// var pos
+button.addEventListener('click', goSendTheInfo);
+button2.addEventListener('click', displayAbout);
+
+function displayAbout(){
+  var about = document.getElementById('about');
+  if (about.style.display === 'none') {
+        about.style.display = 'inline';
+    } else {
+        about.style.display = 'none';
+    }
+}
 
 //fake info from client to client
 function goGetTheInfo() {
-
   var request = new XMLHttpRequest();
-
   request.addEventListener('load', showInfo);
   request.open("GET", '/weather');
   request.send();
+}
 
+function goSendTheInfo(){
+  var request = new XMLHttpRequest();
+  // request.addEventListener('load', goGetTheInfo)
   request.open('POST', '/weather');
-  request.setRequestHeader('Content-type', 'application/x-ww-form-urlencoded');
+  request.setRequestHeader('Content-type', 'application/json');
+  request.addEventListener('load', showInfo);
   request.send(coord);
 
 }
 
+
 function showInfo() {
   // display.innerHTML = this.responseText;
-
   var content = JSON.parse(this.responseText);
-
   console.log(content);
 
-
-  var newPara = document.createElement("p");
-  var text = document.createTextNode("The weather in " + content.current_observation.display_location.full + " on " + content.current_observation.observation_time_rfc822 + " is " + content.current_observation.weather + " and it feels like " + content.current_observation.feelslike_string);
-  newPara.appendChild(text);
-  document.getElementById('weather').appendChild(newPara);
+  // var newPara = document.createElement("p");
+  // var text = document.createTextNode("The Weather in " + content.current_observation.display_location.full + " " + content.current_observation.observation_time + " is " + content.current_observation.weather + " and it feels like " + content.current_observation.feelslike_string);
+  var text = "The Weather in " + content.current_observation.display_location.full + " " + content.current_observation.observation_time + " is " + content.current_observation.weather + " and it feels like " + content.current_observation.feelslike_string
+  // newPara.appendChild(text);
+  document.getElementById('weather').innerHTML = text;
 }
 
 function myMap() {
   var mapProp = {
     center: new google.maps.LatLng(41.13999939, -73.34999847),
-    zoom: 2,
+    zoom: 10,
   };
   var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
 
   google.maps.event.addListener(map, 'click', function(event) {
-   var pos = event.latLng;
-  coord = pos.replace(/[(,)]/g, "");
-   alert(coord);  // in event.latLng  you have the coordinates of click
-  // alert(pos);
-});
-
-infoWindow = new google.maps.InfoWindow;
-
-// Try HTML5 geolocation.
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(function(position) {
-    var pos = {
-      lat: position.coords.latitude,
-      lng: position.coords.longitude
-    };
-
-    infoWindow.setPosition(pos);
-    infoWindow.setContent('Location found.');
-    infoWindow.open(map);
-    map.setCenter(pos);
-  }, function() {
-    handleLocationError(true, infoWindow, map.getCenter());
+    var pos = event.latLng; // in event.latLng  you have the coordinates of click
+    // var params = JSON.stringify()
+    coord = JSON.stringify(pos);
+    // coord = stringify.replace(/{"lat":/g, "").replace(/"lng":/g, "").replace(/}/g, "")
+    console.log(coord); //variable that gets rid of the unwanted characters
   });
-} else {
-  // Browser doesn't support Geolocation
-  handleLocationError(false, infoWindow, map.getCenter());
- }
+
+  infoWindow = new google.maps.InfoWindow;
+
+  // Going to give current location
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Location found.');
+      infoWindow.open(map);
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
 }
--- server.js
+++ server.js
@@ -1,28 +1,33 @@
 var XMLHttpRequest = require("XMLHttpRequest").XMLHttpRequest;
+var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
   res.sendfile('index.html');
+  res.sendfile('index.css');
   res.sendfile('myScript.js');
+  res.sendfile('favicon.ico');
 
 });
 
 //Now I am playing third party server role
-app.get('/weather', function(req, res) {
-  // console.log(event.latLng);
+app.post('/weather', function(req, res) {
+  // console.log("http://api.wunderground.com/api/1a536450bc144857/conditions/q/" + req.body.lat + ',' + req.body.lng + ".json");
   var sRequest = new XMLHttpRequest();
   sRequest.addEventListener('load', sendDataBackToClient);
-
-  sRequest.open("GET", "http://api.wunderground.com/api/1a536450bc144857/forecast/q/" + coord + ".json");
+  sRequest.open("GET", "http://api.wunderground.com/api/1a536450bc144857/conditions/q/" + req.body.lat + ',' + req.body.lng + ".json");
   sRequest.send();
 
 
   function sendDataBackToClient() {
-
     var dataFromOutsideWorld = this.responseText;
+    console.log(dataFromOutsideWorld);
     res.send(dataFromOutsideWorld);
   }
 
@@ -31,4 +36,24 @@ app.get('/weather', function(req, res) {
 
 app.listen(3000, function() {
   console.log('Example app listening on port 3000!');
+});
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index')
+});
+
+app.get('/cool', function(request, response) {
+  response.send(cool());
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });

1dc56e1b62ec23616f3462b964804031b4b153e1
Quinn Wagner <Quinn@kerrys-mbp-3.westport.k12.ct.us>
2017-12-13 12:50:49 -0500

    First Commit

--- server.js
+++ server.js
@@ -2,10 +2,9 @@ var XMLHttpRequest = require("XMLHttpRequest").XMLHttpRequest;
 var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
-
 var bodyParser = require('body-parser');
-app.use(bodyParser.json());
 
+app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
@@ -18,7 +17,7 @@ app.get('/', function(req, res) {
 
 //Now I am playing third party server role
 app.post('/weather', function(req, res) {
-  // console.log("http://api.wunderground.com/api/1a536450bc144857/conditions/q/" + req.body.lat + ',' + req.body.lng + ".json");
+  console.log("http://api.wunderground.com/api/1a536450bc144857/conditions/q/" + req.body.lat + ',' + req.body.lng + ".json");
   var sRequest = new XMLHttpRequest();
   sRequest.addEventListener('load', sendDataBackToClient);
   sRequest.open("GET", "http://api.wunderground.com/api/1a536450bc144857/conditions/q/" + req.body.lat + ',' + req.body.lng + ".json");
@@ -27,7 +26,7 @@ app.post('/weather', function(req, res) {
 
   function sendDataBackToClient() {
     var dataFromOutsideWorld = this.responseText;
-    console.log(dataFromOutsideWorld);
+    // console.log(dataFromOutsideWorld);
     res.send(dataFromOutsideWorld);
   }
 
